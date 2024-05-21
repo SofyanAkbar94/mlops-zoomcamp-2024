3f5a9577cd1b52337baf14481bde115ceb6c92db
SofyanAkbar94 <136363515+SofyanAkbar94@users.noreply.github.com>
2024-05-21 14:29:06 +0000

    add py files and others

---
+++ 02-experiment-tracking/train.py
@@ -0,0 +1,51 @@
+import os
+import pickle
+import click
+import mlflow
+import mlflow.sklearn
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    # Start MLflow run
+    with mlflow.start_run():
+        # Enable autologging
+        mlflow.sklearn.autolog()
+
+        # Create and train the model
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        # Calculate RMSE
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+        # Log RMSE
+        mlflow.log_metric("rmse", rmse)
+
+        # Print RMSE
+        print(f"RMSE: {rmse}")
+
+        # Print the value of min_samples_split
+        min_samples_split = rf.min_samples_split
+        print(f"min_samples_split: {min_samples_split}")
+
+if __name__ == '__main__':
+    run_train()
