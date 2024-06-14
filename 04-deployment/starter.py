e1ec5d1c60d1f1b2fa3248ba868a8c88d3748d6b
SofyanAkbar94 <leethan046@gmail.com>
2024-06-15 01:19:01 +0700

    update ipynb file

--- 04-deployment/starter.py
+++ 04-deployment/starter.py
@@ -1,34 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().system('pip freeze | grep scikit-learn')
-
-
-# In[2]:
-
-
-get_ipython().system('python -V')
-
-
-# In[3]:
-
-
 import pickle
 import pandas as pd
-
-
-# In[4]:
-
+import argparse
+import numpy as np
 
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
-
-
-# In[7]:
-
 
 categorical = ['PULocationID', 'DOLocationID']
 
@@ -44,69 +20,36 @@ def read_data(filename):
     
     return df
 
+def main(year, month):
+    file_path = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    
+    # Load and process the data
+    df = read_data(file_path)
+    
+    # Convert categorical columns to dictionaries and transform
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    
+    # Predict the durations
+    y_pred = model.predict(X_val)
+    
+    # Create an artificial ride_id column
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
-# In[8]:
+    # Create a results dataframe
+    df_result = pd.DataFrame({
+        'ride_id': df['ride_id'],
+        'predicted_duration': y_pred
+    })
 
+    # Calculate and print the mean predicted duration
+    mean_pred_duration = np.mean(y_pred)
+    print(f"Mean predicted duration: {mean_pred_duration}")
 
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--year', type=int, required=True, help='Year of the data')
+    parser.add_argument('--month', type=int, required=True, help='Month of the data')
 
-
-# In[9]:
-
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
-
-
-# In[10]:
-
-
-import numpy as np
-
-std_dev_pred = np.std(y_pred)
-print("Standard Deviation of Predicted Duration:", std_dev_pred)
-
-
-# In[14]:
-
-
-# Define year and month
-year = 2023
-month = 3
-
-# Create an artificial ride_id column
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-
-# In[15]:
-
-
-# Create a results dataframe
-df_result = pd.DataFrame({
-    'ride_id': df['ride_id'],
-    'predicted_duration': y_pred
-})
-
-# Define the output file path
-output_file = 'predictions.parquet'
-
-# Save the results to a Parquet file
-df_result.to_parquet(
-    output_file,
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
-# Check the size of the output file
-import os
-file_size = os.path.getsize(output_file)
-print(f"Size of the output file: {file_size/1024/1024} MB")
-
-
-# In[ ]:
-
-
-
-
+    args = parser.parse_args()
+    main(args.year, args.month)
